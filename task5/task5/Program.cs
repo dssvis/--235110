30e68262e96eb144625fee647ba55a79515d5c48
dssvis <ishkova07.2005@gmail.com>
2024-10-13 20:44:24 +0500

    задание 5

---
+++ task5/task5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = F(1, 2, 3, 4);
+            Console.WriteLine($"x = {x:F3}");
+
+            Console.ReadKey();
+        }
+
+        static double F(double a, double b, double c, double d)
+        {
+            return Math.Sin(a + Math.Sin(b + Math.Sin(c + Math.Sin(d))));
+        }
+    }
+}
+
