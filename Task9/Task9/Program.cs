8e91a11fe3242cee7b9352ab9fc56582c8518336
dssvis <ishkova07.2005@gmail.com>
2024-12-07 02:13:04 +0500

    Задание выполнено

---
+++ Task9/Task9/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            if (!TryInputNumber("Введите число n", out n))
+            {
+                Console.ReadKey();
+                return;
+            }
+
+            if (n <= 0)
+            {
+                Console.WriteLine("Число n должно быть положительным");
+                Console.ReadKey();
+                return;
+            }
+
+            double step = Math.PI / n;
+            Console.WriteLine("Таблица значений функции sin(x):");
+            for (double x = 0; x <= 2 * Math.PI; x += step)
+            {
+                Console.WriteLine($"sin({x:F2}) = {Math.Sin(x):F4}");
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool TryInputNumber(string message, out int number)
+        {
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+
+            if (!int.TryParse(input, out number))
+            {
+                Console.WriteLine("Ошибка ввода");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
+        
+    
+
---
+++ Task9/Task9/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Task9")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Task9")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9ab0ee61-68de-4a02-ae38-d0dc295afef8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
