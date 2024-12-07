49359b07df8c66ff5de5331bd01c0121955d5b63
dssvis <ishkova07.2005@gmail.com>
2024-12-08 01:29:49 +0500

    Задание 10.4

---
+++ task10.4/task10.4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a:");
+            if (!int.TryParse(Console.ReadLine(), out int a))
+            {
+                Console.WriteLine("Ошибка ввода числа a.");
+                return;
+            }
+
+            Console.WriteLine("Введите последовательность целых чисел, заканчивающуюся нулем:");
+            int sum = 0;
+
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Ошибка ввода числа.");
+                    return;
+                }
+
+                if (number == 0) break;
+
+                if (number > a)
+                {
+                    sum += number;
+                }
+            }
+
+            Console.WriteLine($"Сумма всех чисел, больших {a}, равна {sum}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
